ce8f9f97d9b6eafb24e993b54d45f1459a787a40
Nick B <76265064+MrImpossible0@users.noreply.github.com>
2022-02-21 01:37:29 -0500

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,12 @@ namespace Flabby_Bird_Windows_Form
 {
     public partial class Form1 : Form
     {
+
+        int pipeSpeed = 8;
+        int gravity = 10;
+        int score = 0;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -35,6 +41,61 @@ namespace Flabby_Bird_Windows_Form
         private void pipeTop_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            flappyBird.Top += gravity;
+            pipeBottom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+            scoreText.Text = "Score: " + score;
+
+            if(pipeBottom.Left < -150)
+            {
+                pipeBottom.Left = 800;
+                score++;
+            }
+            if(pipeTop.Left < -180)
+            {
+                pipeTop.Left = 950;
+                score++;
+            }
+
+            if(flappyBird.Bounds.IntersectsWith(pipeBottom.Bounds) || 
+                flappyBird.Bounds.IntersectsWith(pipeTop.Bounds) || 
+                flappyBird.Bounds.IntersectsWith(ground.Bounds)
+                )
+            {
+                endGame();
+            }
+        }
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -10;
+            }
+
+
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 10;
+            }
+
+
+        }
+
+        private void endGame()
+        {
+            gameTimer.Stop();
+            scoreText.Text += "Game Over!";
         }
     }
 }
